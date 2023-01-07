aad74e203930575f8c5c88391e46aecba5a0731d
ahmadrosid <alahmadrosid@gmail.com>
2023-01-08 06:26:42 +0700

    chore: tidy up

--- components/editor.tsx
+++ components/editor.tsx
@@ -8,8 +8,6 @@ import {
     createNodeIdPlugin,
     createMentionPlugin,
     createEmojiPlugin,
-    MentionCombobox,
-    MentionElement,
     AutoformatPlugin,
     createAutoformatPlugin
 } from "@udecode/plate";
