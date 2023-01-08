e08bfca52c5429c335af2b9de83d9236c16a7cc0
ahmadrosid <alahmadrosid@gmail.com>
2023-01-08 07:44:59 +0700

    chore: tidy up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
 import Head from 'next/head'
-const PlateEditor = dynamic(() => import('../components/Editor'), {
+const PlateEditor = dynamic(() => import('../components/editor'), {
   ssr: false
 })
-
 
 export default function Index() {
   return (
